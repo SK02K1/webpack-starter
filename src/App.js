5a5ebd5a993cdd0be65116f0e30224bb8f56001d
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-11-20 17:33:06 +0530

    chore: add img-asset transformation rule

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Tanay from './tanay.gif';
 
 const App = ({ header }) => {
   return `
     <div class="App">
       <h2>${header}</h2>
+      <img 
+        class="tanay" 
+        src=${Tanay} 
+        alt="omg-tanay" 
+      />
     </div>
   `;
 };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,6 +33,10 @@ const webpackConfig = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
