00a0c3cb0ada199394b2a13bebc06a23929332de
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-11-21 18:55:21 +0530

    chore: separate vendor bundle for deps coming from node_modules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,7 +5,7 @@ const webpackConfig = {
   entry: path.resolve(__dirname, '../', 'src', 'index.js'),
 
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../', 'dist'),
     clean: true,
   },
@@ -48,6 +48,19 @@ const webpackConfig = {
         type: 'asset',
       },
     ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        node_vendors: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          priority: 1,
+        },
+      },
+    },
   },
 };
 
